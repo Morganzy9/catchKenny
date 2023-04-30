779b2ed38082c395d365a2960d5227d356941a25
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-04-30 10:09:51 +0500

    Initial Commit

---
+++ catchKenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  catchKenny
+//
+//  Created by Ислам Пулатов on 4/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
